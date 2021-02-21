f962a1edb3707252058e2eacfc9eff6f12781c1e
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-20 22:55:59 -0600

    updated prototype file

--- 3_prototypes.js
+++ 3_prototypes.js
@@ -2,18 +2,20 @@
 // ES5
 
 function Book(_title, _author, _year) {
-    // console.log("Book Instantiated...");
     this.title = _title;
     this.author = _author;
     this.year = _year;
+}
 
-    this.getSummary = function() {
-        return `${this.title} was written by ${this.author} in ${this.year}.`;
-    }
+// instead of putting getSummary() in constructor - create a prototype method
+// create a prototype if we don't want a getSummary for every book
+Book.prototype.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}.`;
+
 }
 
 // Instantiate an Object
 const book1 = new Book("Book One", "John Doe", 2013);
 const book2 = new Book("Book Two", "Jane Doe", 2016);
 
-console.log(book1.getSummary());
+console.log(book2);
