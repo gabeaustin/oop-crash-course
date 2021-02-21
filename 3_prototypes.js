9100bd894f900293dd2c29494d1ebe441a556de0
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-20 23:07:11 -0600

    updated prototype file

--- 3_prototypes.js
+++ 3_prototypes.js
@@ -11,11 +11,27 @@ function Book(_title, _author, _year) {
 // create a prototype if we don't want a getSummary for every book
 Book.prototype.getSummary = function() {
     return `${this.title} was written by ${this.author} in ${this.year}.`;
+};
 
+// get age
+Book.prototype.getAge = function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old.`;
+};
+
+// maniuplate the data
+// Revise / Change Year
+Book.prototype.revise = function(newYear) {
+    this.year = newYear;
+    this.revied = true;
 }
+
 
 // Instantiate an Object
 const book1 = new Book("Book One", "John Doe", 2013);
 const book2 = new Book("Book Two", "Jane Doe", 2016);
+const book3 = new Book("Book Three with Age", "Age You", 2001);
 
+console.log(book2);
+book2.revise(2018);
 console.log(book2);
